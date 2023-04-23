b0f1653bd9b6b18341ed041286937879e000d289
Oleg Loutchansky <luchansodev@gmail.com>
2023-04-23 23:28:07 +0300

    feat: add ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
-import * as tf from "@tensorflow/tfjs";
+import * as tf from "@tensorflow/tfjs-node-gpu";
 
 // Define a model for linear regression.
 const model = tf.sequential();
-model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+model.add(
+    tf.layers.dense({ units: 100, activation: "relu", inputShape: [10] })
+);
+model.add(tf.layers.dense({ units: 1, activation: "linear" }));
+model.compile({ optimizer: "sgd", loss: "meanSquaredError" });
 
-model.compile({ loss: "meanSquaredError", optimizer: "sgd" });
+const xs = tf.randomNormal([100, 10]);
+const ys = tf.randomNormal([100, 1]);
 
-// Generate some synthetic data for training.
-const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
-
-// Train the model using the data.
-model.fit(xs, ys, { epochs: 10 }).then(() => {
-    // Use the model to do inference on a data point the model hasn't seen before:
-    console.log(model.predict(tf.tensor2d([5], [1, 1])).toString());
-    // Open the browser devtools to see the output
+// Train the model.
+model.fit(xs, ys, {
+    epochs: 100,
+    callbacks: {
+        onEpochEnd: (epoch, log) =>
+            console.log(`Epoch ${epoch}: loss = ${log?.loss}`),
+    },
 });
