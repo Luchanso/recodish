9ca1527ea8f8fb938ff4e8382960577d98daf313
Oleg Loutchansky <luchansodev@gmail.com>
2023-04-27 00:11:28 +0300

    feat: add sql

--- src/loader.ts
+++ src/loader.ts
@@ -1,2 +1,33 @@
 import * as dotenv from "dotenv";
 dotenv.config();
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const a = 255;
+
+    const b = Buffer.from([a]);
+
+    await prisma.match.create({
+        data: {
+            teamDire: b,
+            duration: 0,
+            id: "0",
+            radiantWin: true,
+            startTime: new Date(),
+            teamRadient: b,
+        },
+    });
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
